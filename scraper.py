9c00dcf60dad0cbe85494ca1eef34dae39b6558a
Josh Espinoza <joshespi@gmail.com>
2023-08-06 20:39:43 -0600

    changed storage to an remote sql server

--- scraper.py
+++ scraper.py
@@ -1,11 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import json
 import time
-from dotenv import load_dotenv
 from datetime import datetime
 import os
-
+import mysql.connector
+from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,6 +16,31 @@ redfin_url = os.getenv("REDFIN_URL")
 if zillow_url is None or redfin_url is None:
     raise ValueError("One or both URLs are missing. Please check the .env file.")
 
+# Establish a connection to the MySQL database
+db_connection = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    port=os.getenv("DB_PORT")
+)
+db_cursor = db_connection.cursor()
+
+# Define the table creation query
+create_table_query = """
+CREATE TABLE IF NOT EXISTS property_values (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    property_name VARCHAR(255),
+    redfin_value VARCHAR(255),
+    zillow_value VARCHAR(255),
+    last_updated DATETIME,
+    UNIQUE (property_name)
+)
+"""
+
+# Execute the table creation query
+db_cursor.execute(create_table_query)
+db_connection.commit()
 
 # Send a GET request to Redfin webpage with headers to mimic a browser
 redfin_headers = {
@@ -36,23 +60,45 @@ zillow_headers = {
 }
 zillow_response = requests.get(zillow_url, headers=zillow_headers)
 zillow_soup = BeautifulSoup(zillow_response.content, 'html.parser')
-zestimate_element = zillow_soup.find('span', class_='Text-c11n-8-89-0__sc-aiai24-0 cfmKEe')
+zestimate_element = zillow_soup.find('span', class_='Text-c11n-8-84-3__sc-aiai24-0 hqOVzy')
 zestimate_value = zestimate_element.find('span').text.strip() if zestimate_element else 'N/A'
 
+# Get the property name (you need to define how you'll obtain this)
+property_name = os.getenv("PROP_ADD")
 
 # Create a timestamp for the last updated time
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Store values and timestamp in a dictionary
-values = {
-    'redfin': redfin_value,
-    'zillow': zestimate_value,
-    'last_updated': timestamp
-}
+# Check if the property_name already exists in the database
+check_query = "SELECT property_name FROM property_values WHERE property_name = %s"
+db_cursor.execute(check_query, (property_name,))
+existing_property = db_cursor.fetchall()
 
-# Save values to a JSON file
-filename = 'data/property_values.json'
-with open(filename, 'w') as file:
-    json.dump(values, file)
+if existing_property:
+    try:
+        # Update the existing row
+        update_query = """
+        UPDATE property_values
+        SET redfin_value = %s, zillow_value = %s, last_updated = %s
+        WHERE property_name = %s
+        """
+        update_values = (redfin_value, zestimate_value, timestamp, property_name)
+        db_cursor.execute(update_query, update_values)
+        db_connection.commit()
+        print("Property values updated in the database.")
+    except Exception as e:
+        print("Error updating property values:", str(e))
+else:
+    # Insert a new row
+    insert_query = """
+    INSERT INTO property_values (property_name, redfin_value, zillow_value, last_updated)
+    VALUES (%s, %s, %s, %s)
+    """
+    insert_values = (property_name, redfin_value, zestimate_value, timestamp)
+    db_cursor.execute(insert_query, insert_values)
+    db_connection.commit()
+    print("New property values saved to the database.")
 
-print("Property values saved to", filename)
+# Close the database cursor and connection
+db_cursor.close()
+db_connection.close()
